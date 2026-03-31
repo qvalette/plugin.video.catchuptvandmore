20a23d87bcc57f7073a5eeec1c7e718bdbecee31
qvalette <quentin@valette.lu>
2026-04-01 00:44:45 +0200

    Add: Introduce 'ro_live' channel to the live TV menu with appropriate routing and metadata

--- resources/lib/skeletons/live_tv.py
+++ resources/lib/skeletons/live_tv.py
@@ -216,5 +216,12 @@ menu = {
         'thumb': 'channels/ve.png',
         'enabled': True,
         'order': 27
+    },
+    'ro_live': {
+        'route': '/resources/lib/main:generic_menu',
+        'label': 30077,
+        'thumb': 'channels/ro.png',
+        'enabled': True,
+        'order': 28
     }
 }
